f746d21d58e02cea16358cd5bacfc7985fc4c069
Aidan Musnitzky <musnit@gmail.com>
2015-11-05 16:01:19 +0200

    Change to single contact picker for now, while multiple search is bugged

--- src/ios/Contactsios9.m
+++ src/ios/Contactsios9.m
@@ -18,11 +18,11 @@
     }];
 }
 
-- (void)contactPicker:(CNContactPickerViewController *)picker
-     didSelectContacts:(NSArray<CNContact *> *)contacts
+/*- (void)contactPicker:(CNContactPickerViewController *)picker
+    didSelectContacts:(NSArray<CNContact *> *)contacts
 {
     NSMutableArray *contactArray = [NSMutableArray array];
-
+    
     for (CNContact* contact in contacts) {
         NSString *thumbnailImage = @"";
         if (contact.imageDataAvailable){
@@ -50,24 +50,71 @@
         }
         
         NSDictionary *contactDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
-                                         contact.givenName, @"givenName",
-                                         contact.middleName, @"middleName",
-                                         contact.familyName, @"familyName",
-                                         phone, @"phone",
-                                         email, @"email",
-                                         linkedin, @"linkedIn",
-                                         twitter, @"twitter",
-                                         thumbnailImage, @"photo",
-                                         nil];
+                                           contact.givenName, @"givenName",
+                                           contact.middleName, @"middleName",
+                                           contact.familyName, @"familyName",
+                                           phone, @"phone",
+                                           email, @"email",
+                                           linkedin, @"linkedIn",
+                                           twitter, @"twitter",
+                                           thumbnailImage, @"photo",
+                                           nil];
         [contactArray addObject:contactDictionary];
     }
     
-
-
+    
+    
     CDVPluginResult* result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
                                messageAsArray:contactArray
                                ];
+    [self success:result callbackId:self.command.callbackId];
+}*/
+
+- (void)contactPicker:(CNContactPickerViewController *)picker
+    didSelectContact:(CNContact *)contact
+{
+    NSString *thumbnailImage = @"";
+    if (contact.imageDataAvailable){
+        thumbnailImage = [contact.thumbnailImageData cdv_base64EncodedString];
+    }
+    
+    NSString *email = @"";
+    if ([contact.emailAddresses count] > 0) {
+        email = contact.emailAddresses[0].value;
+    }
+    
+    NSString *phone = @"";
+    if ([contact.phoneNumbers count] > 0) {
+        phone = contact.phoneNumbers[0].value.stringValue;
+    }
+    NSString *linkedin = @"";
+    NSString *twitter = @"";
+    for (CNLabeledValue* socialProfile in contact.socialProfiles) {
+        if([socialProfile.label isEqualToString:@"twitter"]){
+            twitter = ((CNSocialProfile*)socialProfile.value).urlString;
+        }
+        if([socialProfile.label isEqualToString:@"linkedin"]){
+            linkedin = ((CNSocialProfile*)socialProfile.value).urlString;
+        }
+    }
+    
+    NSDictionary *contactDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                       contact.givenName, @"givenName",
+                                       contact.middleName, @"middleName",
+                                       contact.familyName, @"familyName",
+                                       phone, @"phone",
+                                       email, @"email",
+                                       linkedin, @"linkedIn",
+                                       twitter, @"twitter",
+                                       thumbnailImage, @"photo",
+                                       nil];
+    
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsDictionary:contactDictionary
+                               ];
+
     [self success:result callbackId:self.command.callbackId];
 }
 
